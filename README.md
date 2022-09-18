ae22170b89f4a97da9553bcdbe80cfc3e578ba05
artem <artem.maximov.2000@bk.ru>
2022-09-18 10:59:28 +0300

    фикс вывода

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Сборнику учебных драйвер под ОС Linux 
 
 OC - Linux Mint
+
+
 Kernel version - 5.4.0-125
--- miet.c
+++ miet.c
@@ -2,20 +2,21 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Maximov Artem");
+MODULE_DESCRIPTION("Module for education");
+MODULE_VERSION("1.0");
+
 static int __init ModuleInit(void) {
-	printk(KERN_INFO "Hello world!");
+	printk(KERN_INFO "Hello world!\n");
 	return 0;
 }
 module_init(ModuleInit);
 
 static void  __exit ModuleExit(void) {
-	printk(KERN_INFO "Exit module");
+	printk(KERN_INFO "Exit module\n");
 }
 
 
 module_exit(ModuleExit);
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Maximov Artem");
-MODULE_DESCRIPTION("Module for education");
-MODULE_VERSION("1.0");
