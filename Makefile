96e5ad3ebcff9a6fc18e0418401c9297a4240c57
artem <artem.maximov.2000@bk.ru>
2022-10-01 18:03:23 +0300

    фикс передачи параметров

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-obj-m += miet.o param.o
+obj-m += miet.o
 
 
 KDIR = /lib/modules/$(shell uname -r)/build
--- miet.c
+++ miet.c
@@ -2,31 +2,41 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
-#include "param.h"
 
 uint8_t getParams(void);
 
-static int __init ModuleInit(void) {
+static int integer = 0;
+module_param(integer, int, S_IRUSR | S_IWUSR);
+
+uint8_t getParams(void)
+{
+	uint8_t ret = 0;
+	printk(KERN_INFO "Intger argument int: %d\n", integer);
+
+	return ret;
+}
+
+static int __init ModuleInit(void)
+{
 	uint8_t ret = 0;
 	printk(KERN_INFO "Started loading module\n");
 	printk(KERN_INFO "Success loading module!\n");
 
-
 	ret = getParams();
-	if(ret) {
+	if (ret) {
 		printk(KERN_INFO "Error invalid arguments!\n");
 		printk(KERN_INFO "Started unloading module\n");
 	}
-	return ret; 
+	return ret;
 }
 
-static void  __exit ModuleExit(void) {
+static void __exit ModuleExit(void)
+{
 	printk(KERN_INFO "Success unloading module!\n");
 }
 
 module_init(ModuleInit);
 module_exit(ModuleExit);
-
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Maximov Artem");
